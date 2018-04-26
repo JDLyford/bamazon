532a9f3fedf5237c580119c61362079c4b0403d6
Joseph Lyford <lyford.joseph@yahoo.com>
2018-04-26 15:05:33 -0400

    edited Customer and Manager js files to be more user friendly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
+// Required npms
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Setting up the connection
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,11 +11,14 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-
+// Starts the program
 begin();
 
+// Start function
 function begin() {
+    // Calls function to display products
     showAll();
+    // Sets loc to reduce code
     var loc = "SELECT*FROM bamazon.products";
     var myuserReq;
     connection.query(loc, function (err, results) {
@@ -29,14 +34,17 @@ function begin() {
                 message: "How many do you want?"
             }
         ]).then(function (userReq) {
+            // Checks to see if valid reques
             for (var i = 0; i < results.length; i++) {
                 if (results[i].id == userReq.id) {
                     myuserReq = results[i];
-
+                    
+                    // If the request is greater than stock then backout of function and restart
                     if (results[i].stock_quantity < userReq.quantity) {
                         console.log("We out, stop asking");
-                        begin();
+                        begin()
                     } else if (results[i].stock_quantity >= userReq.quantity) {
+                        // If the request is less than stock then update data and return results from functions
                         price(userReq.quantity, myuserReq.price);
                         purchase(results[i].stock_quantity, userReq.quantity, userReq.id);
                     }
@@ -46,6 +54,7 @@ function begin() {
     })
 };
 
+// Basic function to display the result of user purchase
 function price(quan, p) {
     var total = quan * p;
     console.log("each cost $" + p);
@@ -53,6 +62,7 @@ function price(quan, p) {
     console.log("Your TOTAL is: $" + total);
 };
 
+// Updates the database and ends the connection
 var newQuan = 0;
 function quanUpdate(a, b, c) {
     newQuan = a - b;
@@ -66,6 +76,7 @@ function quanUpdate(a, b, c) {
     connection.end();
 };
 
+// Displays all products from the database
 function showAll() {
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
@@ -78,6 +89,7 @@ function showAll() {
     });
 }
 
+// Confirmation function to proceed with the purchase and the finalize the transaction
 function purchase(a, b, c) {
     inquirer.prompt([{
         name: "yesorno",
@@ -85,6 +97,7 @@ function purchase(a, b, c) {
         message: "Do you want to make this purchase?",
         choices: ["YES", "NO"]
     }]).then(function (ans) {
+        // Runs another function to update quanty if Yes or restarts if no
         if (ans.yesorno.toUpperCase() === "YES") {
             quanUpdate(a, b, c);
         } else {
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,6 +1,8 @@
+// Required npms to run
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Initial connection to mysql
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,14 +11,17 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+// Starting function
 manage();
+// Function that allows user to choose what they want to do
 function manage() {
     inquirer.prompt([{
         name: "manage",
         type: "rawlist",
-        message: "Wat do you want!",
+        message: "What would you like?",
         choices: ["Product for sale", "Low Inventory", "Add to Inventory", "Add New Product"]
     }]).then(function (ans) {
+        // Switch to split program into four main areas and thier functions
         switch (ans.manage) {
             case "Product for sale":
                 forSale();
@@ -34,22 +39,25 @@ function manage() {
     })
 };
 
+// Function that displays current products
 function forSale() {
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-
-        console.log('-------------------------------------------------');
-        for (var i = 0; i < res.length; i++) {
-            console.log("Id: " + res[i].id + " -- " +
-                res[i].product_name + " -- " +
-                res[i].department_name + " -- " +
-                res[i].price + " -- " +
-                res[i].stock_quantity);
-        }
-    })
+    showAll();
     connection.end();
 };
 
+function showAll() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+
+        console.log("---------------------------------------------");
+        for (var i = 0; i < results.length; i++) {
+            console.log("Id: " + results[i].id + " -- " + results[i].product_name + " -- "
+                + results[i].department_name + " -- " + results[i].price + "-- " + results[i].stock_quantity);
+        }
+    });
+}
+
+// Displays all inventory that has a quantity less than 5
 function lowInv() {
     var lowInvList = [];
     connection.query("SELECT * FROM products", function (err, res) {
@@ -70,10 +78,11 @@ function lowInv() {
     connection.end();
 };
 
-
+// Function that allows user to input quantity through prompts
 function addInv() {
     var newAmount;
     var newInvt;
+    showAll();
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         inquirer.prompt([{
@@ -83,8 +92,9 @@ function addInv() {
         }, {
             name: "amountAdd",
             type: "input",
-            message: "How many does it have now?"
+            message: "How much would you like to add?"
         }]).then(function (ans) {
+            // Adds input to current amount
             for (var i = 0; i < res.length; i++) {
                 if (res[i].id == ans.productIdN) {
                     newAmount = res[i];
@@ -103,7 +113,7 @@ function addInv() {
     })
 };
 
-
+// Function allowing user to add a new product through prompts
 function addNewProduct() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
@@ -124,6 +134,7 @@ function addNewProduct() {
             type: "input",
             message: "What is the stock amount"
         }]).then(function (ans) {
+            // Inserts new product into database
             connection.query("INSERT INTO products SET ?", {
                 product_name: ans.pname,
                 department_name: ans.dname,
@@ -132,6 +143,7 @@ function addNewProduct() {
             }, function (err) {
                 if (err) throw err;
                 console.log("Product added");
+                showAll();
                 connection.end();
             })
         })
